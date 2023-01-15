b08398e403171881e5a71f283f3b74fb5921d0dc
Mayranne <mayranne-@hotmail.com>
2023-01-15 19:00:03 +0000

    fixed probs

--- src/components/Sections/Contact/Contact.jsx
+++ src/components/Sections/Contact/Contact.jsx
@@ -2,6 +2,7 @@
 import "./Contact.scss";
 import memoji from "../../../assets/memoji-star.png";
 // Libraries
+import { NetlifyForm, Honeypot } from "react-netlify-forms";
 // Components
 //////////////////////////////////////////////////////////////
 
@@ -14,41 +15,65 @@ const Contact = () => {
         </div>
         <div className="categories-body">
           <div className="categories-body-left">
-            <a>
+            <a href="https://github.com/heydut" target="_blank">
               <i class="fa-brands fa-github" />
             </a>
-            <a>
+            <a href="https://www.linkedin.com/in/mayranne/" target="_blank">
               <i class="fa-brands fa-linkedin-in" />
             </a>
-            <a>
+            <a
+              onClick={() =>
+                navigator.clipboard.writeText("mayranne-@hotmail.com")
+              }
+            >
               <i class="fa-solid fa-envelope" />
             </a>
-            <a>
+            <a href="https://wa.me/447746193982" target="_blank">
               <i class="fa-brands fa-whatsapp" />
             </a>
             <img src={memoji} alt="memoji" />
           </div>
-          <form
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            action="/"
-            className="categories-body-right"
-          >
-            <div>
-              <input type="text" placeholder="Your name" name="name"></input>
-              <input type="text" placeholder="Your email" name="email"></input>
-            </div>
-            <textarea
-              name="message"
-              type="text"
-              placeholder="Write your message here..."
-              className="categories-body-message"
-            ></textarea>
-            <button type="submit" value="Submit">
-              Submit
-            </button>
-          </form>
+          <NetlifyForm name="contact" honeypotName="bot-field">
+            {({ handleChange, success, error }) => (
+              <div className="categories-body-right">
+                <Honeypot />
+                {success && <p>Thanks for contacting us!</p>}
+                {error && (
+                  <p>
+                    Sorry, we could not reach our servers. Please try again
+                    later.
+                  </p>
+                )}
+
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Your name"
+                    name="name"
+                    onChange={handleChange}
+                  ></input>
+                  <input
+                    type="text"
+                    placeholder="Your email"
+                    name="email"
+                    onChange={handleChange}
+                  ></input>
+                </div>
+                <textarea
+                  name="message"
+                  type="text"
+                  placeholder="Write your message here..."
+                  className="categories-body-message"
+                  onChange={handleChange}
+                ></textarea>
+                <button type="submit" value="Submit">
+                  Submit
+                </button>
+              </div>
+            )}
+
+            {/* ************ */}
+          </NetlifyForm>
         </div>
       </div>
     </section>
